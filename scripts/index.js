090bb60fa811df8ba9a849c2a259f961022eca69
Anna Yureva <yuanya@yandex.ru>
2022-06-25 18:48:37 +0300

    Итерация 2

--- scripts/index.js
+++ scripts/index.js
@@ -2,20 +2,20 @@ const profileEditButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = popup.querySelector('.popup__close-icon')
 
-let formElement = document.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.popup__username');
-let jobInput = formElement.querySelector('.popup__profession');
+let formElement = document.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__input_first_line');
+let jobInput = formElement.querySelector('.popup__input_second_line');
 let profileUsername = document.querySelector('.profile__username');
 let profileProfession = document.querySelector('.profile__profession');
 
 function openPopup(event) {
-    popup.classList.add('popup__opened')
+    popup.classList.add('popup_opened')
     nameInput.value = profileUsername.textContent
     jobInput.value = profileProfession.textContent
 };
 
 function closePopup(event) {
-    popup.classList.remove('popup__opened')
+    popup.classList.remove('popup_opened')
 };
 
 function formSubmitHandler(event) {
