81419d33030c9e10bf7be759875cc83c17f99da5
Anna Yureva <yuanya@yandex.ru>
2022-08-27 13:40:49 +0300

    initial rendering

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,46 @@ function formSubmitHandler(event) {
 profileEditButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
+
+const initialCards = [
+    {
+      name: 'Северная\u00A0Осетия',
+      photo: 'images/ОзероГижгит.jpg',
+    },
+    {
+      name: 'Озеро\u00A0Гижгит',
+      photo: 'images/ОзероГижгит.jpg'
+    },
+    {
+      name: 'Кадаргаван',
+      photo: 'images/Кадаргаван-min.jpg'
+    },
+    {
+      name: 'Железноводск',
+      photo: 'images/Железноводск-min.jpg'
+    },
+    {
+      name: 'Кисловодск',
+      photo: 'images/Кисловодск-min.jpg'
+    },
+    {
+      name: 'Кабардино-Балкария',
+      photo: 'images/Кабардино-Балкария-min.jpg'
+    }
+  ];
+
+  const listElement = document.querySelector('.elements');
+  const templateElement = document.querySelector('.template');
+
+
+  function addNewElement(name, photo) {
+    const newElement = templateElement.content.cloneNode(true);
+    newElement.querySelector('.elements__name').textContent = name;
+    newElement.querySelector('.elements__photo').src = photo;
+    newElement.querySelector('.elements__photo').setAttribute('alt', name);
+    listElement.prepend(newElement);
+  };
+
+  initialCards.forEach((element) => {
+    addNewElement(element.name, element.photo);
+  });
